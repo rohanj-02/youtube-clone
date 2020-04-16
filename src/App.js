ada1c07a35e13ec6f5c069b35bc3bbe18a2e8f97
Rohan Jain <rohan19095@iiitd.ac.in>
2020-04-17 02:55:44 +0530

    Framework Updated

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Grid } from '@material-ui/core';
+import youtube from './api/youtube';
+import {SearchBar, VideoPlayer, VideoList} from './components/index';
+import config from './api/config';
+
+class App extends React.Component{
+    state = {
+        videos : [],
+        selectedVideo : null,
+    }
+    handleSubmit = async (searchTerm) =>{
+        const response = await youtube.get("search",{ 
+            params : { 
+                part: 'snippet',
+                maxResults: '5',
+                key: config.API_KEY,
+                q : searchTerm,
+            }});
+        console.log(response.data.items);
+        this.setState({videos : response.data.items, selectedVideo : response.data.items[0] });
+    }
+
+    render(){
+        const {selectedVideo, videos} = this.state;
+        return(
+            <Grid justify="center" container spacing = {10}>
+                <Grid item xs={12}>
+                    <Grid container spacing={10}>
+                        <Grid item xs={2}>
+                            <h4> Logo </h4>
+                        </Grid>
+                        <Grid item xs={8}>
+                            <SearchBar onFormSubmit = {this.handleSubmit}/>
+                        </Grid>
+                        <Grid item xs={2}></Grid>
+                        <Grid item xs={1}></Grid>
+                        <Grid item xs={7}>
+                            <VideoPlayer video = {selectedVideo}/>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <VideoList videos = {videos}/>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        )
+    }
+}
+
+export default App;
---
+++ src/components/VideoPlayer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Paper, Typography} from "@material-ui/core";
+
+const VideoPlayer = (props) =>{
+    const {video} = props;
+    return(
+        <h1> Video Player </h1>
+    )
+}
+
+export default VideoPlayer;
